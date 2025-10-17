aaf32f15feb75534a267a2b7984e1c4b889ee583
khanolkarsujal <khanolkarsujal@gmail.com>
2025-10-17 19:54:46 +0530

    day 23 complete

---
+++ day1/main.py
@@ -0,0 +1,7 @@
+print("Day 1 - Python Print function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("hello world!\nhello world!\nhello world!")
+
+print("Hello" +" "+ "Angela") 
