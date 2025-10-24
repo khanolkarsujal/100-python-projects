ad6081b9bab2b766d304846a4991adc93207839a
khanolkarsujal <khanolkarsujal@gmail.com>
2025-10-24 19:23:37 +0530

    Day 1 - Beginner - Working with Variables in Python to Manage Data

---
+++ day01/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+user_input = input("what's name of the city you grew in ?\n")
+user_input2 = input("what's your pet's name?\n")
+print(f"Your band name could be {user_input} {user_input2}")
+
+ 
