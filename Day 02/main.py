69de9ab11d0233a5c22c80798c0718c4eacdccd3
khanolkarsujal <khanolkarsujal@gmail.com>
2025-12-05 10:48:09 +0530

    updated Tip calulator

--- Day 02/main.py
+++ Day 02/main.py
@@ -1,12 +1,24 @@
+import art
+
+#logo Tip Calulator
+print(art.logo)
+print(art.menu_start)
+
 print("Welcome to the tip calculator.")
-bill_amount = float(input("What was the total bill?\n"))
-spilt_bill = float(input("How many people to split the bill?\n"))
-tip = float(input("What percentage tip would you like to give? 10 , 12 , or 15 ? \n"))
-                    
+
+#Taking inputs
+bill_amount = float(input("What was the total bill? :"))
+spilt_bill = float(input("\nHow many people to split the bill? :"))
+tip = float(input("\nWhat percentage tip would you like to give? 10 , 12 , or 15 ? :"))
+
+#Calulations                   
 spilt_amount = float(bill_amount/spilt_bill)
 tip_percetage_amount = spilt_amount*(tip/100)
 final_amount = spilt_amount + tip_percetage_amount
 
-print(f"Each person should pay:${final_amount} ")
+print(art.menu_end) 
+
+print(f"───── ⚔︎  Each person should pay ⚔︎ ───── \n : ${final_amount} ")
+print(art.menu_end)
 
 
