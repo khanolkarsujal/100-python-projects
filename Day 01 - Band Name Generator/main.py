773e45437df12f42311c3a39473081eddf71b520
khanolkarsujal <khanolkarsujal@gmail.com>
2025-12-05 11:25:32 +0530

    Added Clear Function , Loop And Fix UI

--- Day 01 - Band Name Generator/main.py
+++ Day 01 - Band Name Generator/main.py
@@ -1,16 +1,31 @@
 import art
+import os
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+while True :
+    clear()
 #logo
-print(art.logo)
-print(art.menu_start)
+    print(art.logo)
+    print(art.menu_start)
 
 #Questions to generate Band Name
-print("Welcome To The Band Name Generator\n")
-user_input = input("\nWhat's Name Of The City You Grew In ? : ").title()
-user_input2 = input("\nWhat's Your Pet's Name? : ").title()
-print(art.menu_end)
+    print("Welcome To The Band Name Generator\n")
+    user_input = input("\nWhat's Name Of The City You Grew In ? : ").title()
+    user_input2 = input("\nWhat's Your Pet's Name? : ").title()
+    clear()
+    print(art.logo)
+    print(art.menu_end)
 
 
 #Generated Band Name
-print(f"───── ⚔︎ Your Band Name Could Be ⚔︎ ─────: \n\n : ✦⚔︎⚔︎⚔︎ {user_input}.{user_input2} ⚔︎⚔︎⚔︎✦")
-print(art.menu_end)
+    print(f"───── ⚔︎ Your Band Name Could Be ⚔︎ ─────: \n\n : ✦⚔︎⚔︎⚔︎ {user_input}.{user_input2} ⚔︎⚔︎⚔︎✦")
+    print(art.menu_end)
+
+    again = input("Do You Want Continue Generate Another ? : ").lower()
+    if again not in ["yes","y"] :
+        print("Exit...")
+        break
+    else:
+        continue
