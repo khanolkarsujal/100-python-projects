a2f6158d4d7a615028c1926e8d4da17695610f4a
khanolkarsujal <khanolkarsujal@gmail.com>
2025-12-05 10:35:01 +0530

    updated Band Name Generator

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,6 +1,16 @@
-print("Welcome to the Band Name Generator")
-user_input = input("what's name of the city you grew in ?\n")
-user_input2 = input("what's your pet's name?\n")
-print(f"Your band name could be {user_input} {user_input2}")
+import art
 
- 
+#logo
+print(art.logo)
+print(art.menu_start)
+
+#Questions to generate Band Name
+print("Welcome to the Band Name Generator\n")
+user_input = input("\nwhat's name of the city you grew in ? : ").title()
+user_input2 = input("\nwhat's your pet's name? : ").title()
+print(art.menu_end)
+
+
+#Generated Band Name
+print(f"───── ⚔︎ Your Band Name Could Be ⚔︎ ─────: \n\n : ✦⚔︎⚔︎⚔︎ {user_input}.{user_input2} ⚔︎⚔︎⚔︎✦")
+print(art.menu_end)
