659e5bde48ac2162d69926ae3edf9bc5cd153ed1
khanolkarsujal <khanolkarsujal@gmail.com>
2025-12-05 12:31:41 +0530

    Improve Varible Naming To Improve Readable

--- Day 02 - Tip Calulator/main.py
+++ Day 02 - Tip Calulator/main.py
@@ -8,17 +8,17 @@ print("Welcome To The Tip Calculator.")
 
 #Taking inputs
 bill_amount = float(input("What Was The Total Bill? :$"))
-spilt_bill = float(input("\nHow Many People To Split The Bill? :"))
-tip = float(input("\nWhat Percentage(%) Tip Would You Like To Give? 10 , 12 , or 15 ? :%"))
+number_of_people = float(input("\nHow Many People To Split The Bill? :"))
+tip_percetage = float(input("\nWhat Percentage(%) Tip Would You Like To Give? 10 , 12 , or 15 ? :%"))
 
 #Calulations                   
-spilt_amount = float(bill_amount/spilt_bill)
-tip_percetage_amount = spilt_amount*(tip/100)
-final_amount = spilt_amount + tip_percetage_amount
+amount_per_person_before_tip = float(bill_amount/number_of_people)
+tip_per_person = number_of_people*(amount_per_person_before_tip/100)
+final_amount_per_person = amount_per_person_before_tip + tip_per_person
 
 print(art.menu_end) 
 
-print(f"───── ⚔︎  Each Person Should Pay ⚔︎ ───── \n\n : ───── ${final_amount} ───── ")
+print(f"───── ⚔︎  Each Person Should Pay ⚔︎ ───── \n\n : ───── ${final_amount_per_person} ───── ")
 print(art.menu_end)
 
 
